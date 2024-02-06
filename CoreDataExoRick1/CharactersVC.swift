b3f3735bc8adf037b9af2a28099f49673cab66c4
Romek <129655936+romekdomasik@users.noreply.github.com>
2024-02-06 15:15:29 +0100

    ajout des différents écrans

---
+++ CoreDataExoRick1/CharactersVC.swift
@@ -0,0 +1,23 @@
+//
+//  CharactersVC.swift
+//  CoreDataExoRick1
+//
+//  Created by roman domasik on 06/02/2024.
+//
+
+import UIKit
+
+class CharactersVC: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+   
+
+}
