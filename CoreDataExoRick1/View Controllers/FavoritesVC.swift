ec49802dd379ae5347a23184e990b0ebc8bde948
Romek <129655936+romekdomasik@users.noreply.github.com>
2024-02-16 20:17:04 +0100

    tout ok sauf boutton like qui ne s'actualise pas

--- CoreDataExoRick1/View Controllers/FavoritesVC.swift
+++ CoreDataExoRick1/View Controllers/FavoritesVC.swift
@@ -2,28 +2,86 @@
 //  FavoritesVC.swift
 //  CoreDataExoRick1
 //
-//  Created by roman domasik on 06/02/2024.
+//  Created by roman } on 06/02/2024.
 //
 
 import UIKit
+import CoreData
 
-class FavoritesVC: UIViewController {
+class FavoritesVC: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    private var resultsController: NSFetchedResultsController<Favorite>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        let request = Favorite.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        resultsController = NSFetchedResultsController(
+            fetchRequest: request,
+            managedObjectContext: DataManager.shared.context,
+            sectionNameKeyPath: nil,
+            cacheName: nil)
+        resultsController.delegate = self
+        
+        do{
+            try resultsController.performFetch()
+        } catch {
+            print("error while trying to perform fetch", error)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        resultsController.fetchedObjects?.count ?? 0
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let favorites = resultsController.object(at: indexPath)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = favorites.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let favorite = resultsController.object(at: indexPath)
+            DataManager.shared.deleteFavorite(favorite)
+        }
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .automatic)
+        case .move:
+            tableView.deleteRows(at: [indexPath!], with: .automatic)
+            tableView.insertRows(at: [newIndexPath!], with: .automatic)
+        case .update:
+            tableView.reloadRows(at: [indexPath!], with: .automatic)
+        default:
+            break
+        }
+    }
 }
